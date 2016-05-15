6dfc945dccdc99707d067326278bea02ea49595b
Diego Russo <me@diegor.it>
2016-05-15 18:49:11 +0100

    Serialization of JSON data in a separate thread

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -14,11 +14,11 @@ class APIManager{
         
         // An ephemeral session has no persistent disk storage for cookies,
         // cache or credentials.
-        // let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
-        // let session = NSURLSession(configuration: config)
+        let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+        let session = NSURLSession(configuration: config)
         
         // Singleton disegn pattern
-        let session = NSURLSession.sharedSession()
+        //let session = NSURLSession.sharedSession()
         let url = NSURL(string: urlString)!
         
         // Async communication call in background thread
@@ -26,14 +26,35 @@ class APIManager{
         let task = session.dataTaskWithURL(url) {
             (data, response, error) -> Void in
             
-            // Move everything back to the main thread
-            dispatch_async(dispatch_get_main_queue()){
-                if error != nil {
+            if error != nil { // oops, there is an error
+                dispatch_async(dispatch_get_main_queue()) {
                     completion(result: (error!.localizedDescription))
-                } else {
-                    completion(result: "NSURLSession successful")
-                    print(data)
                 }
+            } else { // OK, everything looks good
+                // print(data)
+                // JSONSerialization
+                do {
+                    /* .AllowFragments - top level object is not Array or Dictionary
+                    Any type of string or value
+                    NSJSONSerialization requires the Do / Try /Catch
+                    Converts the NSDATA into a JSON Object and cast it to a Dictionary */
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject] {
+                        
+                        print(json)
+                        
+                        let priority = DISPATCH_QUEUE_PRIORITY_HIGH //highest priority possible
+                        dispatch_async(dispatch_get_global_queue(priority, 0)){
+                            dispatch_async(dispatch_get_main_queue()) {
+                                completion(result: "JSONSerialization Successful")
+                            }
+                        }
+                    }
+                } catch {
+                    dispatch_async(dispatch_get_main_queue()){
+                        completion(result: "error in NSJSONSerialization")
+                    }
+                }
+                // End of JSONSerialization
             }
         }
         // It executes the background process
