2e8d6d184b1aebe93f02a57622935d9fd8b87fa5
Diego Russo <me@diegor.it>
2016-05-18 07:25:47 +0100

    Change the view depending on connection status

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Created pressing ctrl+dragging the label from Main.storyboard to here
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.reachabilityStatusChanged), name: "ReachStatusChanged", object: nil)
+        
+        // Just call it the first time
+        reachabilityStatusChanged()
         
         // Call API
         let api = APIManager()
@@ -34,6 +42,29 @@ class ViewController: UIViewController {
 //        alert.addAction(okAction)
 //        self.presentViewController(alert, animated: true, completion:nil)
         
+    }
+    
+    // method to execute every time the ReachStatusChanged notification is received
+    func reachabilityStatusChanged(){
+        switch reachabilityStatus {
+        case NOACCESS:
+            view.backgroundColor = UIColor.redColor()
+            displayLabel.text = "No Internet"
+        case WIFI:
+            view.backgroundColor = UIColor.greenColor()
+            displayLabel.text = "Reachable with WIFI"
+        case WWAN:
+            view.backgroundColor = UIColor.yellowColor()
+            displayLabel.text = "Reachable with Cellular"
+        default:
+            return
+        }
+    }
+    
+    // It is called just as the object  is about to be deallocated
+    deinit{
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
+    
     }
 }
 
