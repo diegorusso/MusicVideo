68d219c6be100654f7d0548b38b89290c3edafdd
Diego Russo <me@diegor.it>
2016-05-19 23:22:25 +0100

    Add alert when there is not connection

--- MusicVideo/MusicVideoTVC.swift
+++ MusicVideo/MusicVideoTVC.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class MusicVideoTVC: UITableViewController {
-
+    
     var videos = [Videos]()
     
+    // This is called only once
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,10 +21,7 @@ class MusicVideoTVC: UITableViewController {
         // Just call it the first time
         reachabilityStatusChanged()
         
-        // Call API
-        let api = APIManager()
-        let urlApi = "https://itunes.apple.com/uk/rss/topmusicvideos/limit=50/json"
-        api.loadData(urlApi, completion: didLoadData)
+        
     }
     
     func didLoadData(videos: [Videos]) {
@@ -42,16 +40,50 @@ class MusicVideoTVC: UITableViewController {
         switch reachabilityStatus {
         case NOACCESS:
             view.backgroundColor = UIColor.redColor()
-            //displayLabel.text = "No Internet"
-        case WIFI:
-            view.backgroundColor = UIColor.greenColor()
-            //displayLabel.text = "Reachable with WIFI"
-        case WWAN:
-            view.backgroundColor = UIColor.yellowColor()
-            //displayLabel.text = "Reachable with Cellular"
+            // Move back to the main queue
+            dispatch_async(dispatch_get_main_queue()){
+                let alert = UIAlertController(title: "No Internet Access", message: "Please make sure you are connected to the Internet", preferredStyle: .Alert)
+                
+                let cancelAction = UIAlertAction(title: "Cancel", style: .Default){
+                    action -> () in
+                    print("Cancel")
+                }
+                
+                let deleteAction = UIAlertAction(title: "Delete", style: .Destructive){
+                    action -> () in
+                    print("Delete")
+                }
+                
+                let okAction = UIAlertAction(title: "OK", style: .Default){
+                    action -> Void in
+                    print("OK")
+                    
+                    // do something if you want
+                }
+                
+                alert.addAction(okAction)
+                alert.addAction(cancelAction)
+                alert.addAction(deleteAction)
+                
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+            
         default:
-            return
+            view.backgroundColor = UIColor.greenColor()
+            if videos.count > 0 {
+                print("Do not refresh API")
+            } else {
+                runAPI()
+            }
+            
         }
+    }
+    
+    func runAPI() {
+        // Call API
+        let api = APIManager()
+        let urlApi = "https://itunes.apple.com/uk/rss/topmusicvideos/limit=50/json"
+        api.loadData(urlApi, completion: didLoadData)
     }
     
     // It is called just as the object  is about to be deallocated
@@ -59,19 +91,19 @@ class MusicVideoTVC: UITableViewController {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
         
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return videos.count
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
@@ -81,50 +113,50 @@ class MusicVideoTVC: UITableViewController {
         return cell
     }
     
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+     if editingStyle == .Delete {
+     // Delete the row from the data source
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+     } else if editingStyle == .Insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
