569266394858ff4bb839258053a1c7a74786c352
Diego Russo <me@diegor.it>
2016-05-20 08:52:32 +0100

    Method to get music video image

--- MusicVideo/MusicVideoTableViewCell.swift
+++ MusicVideo/MusicVideoTableViewCell.swift
@@ -11,7 +11,7 @@ import UIKit
 class MusicVideoTableViewCell: UITableViewCell {
     
     // The logic to setup the cell should be in this class and nowhere else!
-
+    
     var video: Videos? {
         didSet{
             updateCell()
@@ -25,8 +25,33 @@ class MusicVideoTableViewCell: UITableViewCell {
     func updateCell(){
         musicTitle.text = video?.vName
         rank.text = ("\(video!.vRank)")
-        musicImage.image = UIImage(named: "imageNotAvailable")
-    
+        //musicImage.image = UIImage(named: "imageNotAvailable")
+        
+        if video!.vImageData != nil {
+            print("Getting data from array...")
+            musicImage.image = UIImage(data: video!.vImageData!)
+        } else {
+            GetVideoImage(video!, imageView: musicImage)
+        }
+        
     }
     
+    
+    func GetVideoImage(video: Videos, imageView: UIImageView){
+        // This happens in a separate thread
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)){
+            
+            let data = NSData(contentsOfURL: NSURL(string: video.vImageUrl)!)
+            var image: UIImage?
+            if data != nil {
+                video.vImageData = data
+                image = UIImage(data: data!)
+            }
+            
+            // move back to the main Queue
+            dispatch_async(dispatch_get_main_queue()){
+                imageView.image = image
+            }
+        }
+    }
 }
